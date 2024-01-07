25eb59fd85a65f1107ce5fb2660a1b18a3487cc9
Dean Harding <dean@codeka.com>
2024-01-07 00:05:52 -0800

    Added the beginnings of the login form.

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
 import { ServicesModule } from './services/services.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { ServicesModule } from './services/services.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AuthModule,
     ServicesModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- web/src/app/auth/auth.module.ts
+++ web/src/app/auth/auth.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 
-import { AuthService } from '../services/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ServicesModule } from '../services/services.module';
+
 import { LoginComponent } from './login.component';
 
 @NgModule({
   declarations: [LoginComponent],
-  imports: [AuthService],
+  imports: [
+    ServicesModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule,
+    MatSlideToggleModule],
   providers: [],
   bootstrap: []
 })
-export class ServicesModule { }
+export class AuthModule { }
