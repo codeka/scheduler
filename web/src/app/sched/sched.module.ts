b4b10f793ff339529f38026e7b9baeabb5a1f7a9
Dean Harding <dean@codeka.com>
2024-01-11 21:45:39 -0800

    Started making the 'weekly' view.

--- web/src/app/sched/sched.module.ts
+++ web/src/app/sched/sched.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WeekComponent } from './week.component';
+import { ServicesModule } from '../services/services.module';
 
 @NgModule({
   declarations: [WeekComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ServicesModule],
   providers: [],
   bootstrap: []
 })
--- web/src/app/services/services.module.ts
+++ web/src/app/services/services.module.ts
@@ -1,5 +1,5 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
-import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { AuthInterceptor } from './auth.interceptor';
 import { AuthService } from './auth.service';
